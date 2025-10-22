82abda5d72bff3f4258fecb178e4f3d6f6569cd9
quargsgreene <quargsgreene@gmail.com>
2025-10-22 16:20:13 -0400

    created service root directories

---
+++ IOService/hello-rust/target/debug/deps/hello_rust-5456b8fb3902c89f.d
@@ -0,0 +1,5 @@
+/Users/quargsgreene/code/Trussal/hello-rust/target/debug/deps/hello_rust-5456b8fb3902c89f.d: src/main.rs
+
+/Users/quargsgreene/code/Trussal/hello-rust/target/debug/deps/hello_rust-5456b8fb3902c89f: src/main.rs
+
+src/main.rs:
